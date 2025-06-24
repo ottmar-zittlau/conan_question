8288d29d0f4ad1603f90485cbef32f3c1c080b90
Ottmar Zittlau <ozteopat@gmx.net>
2025-06-24 12:18:58 +0200

    Update recipes

--- run_conan.sh
+++ run_conan.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cd package1
-conan create . --test-folder testpackage
+conan create . --test-folder testpackage --update
 cd -
 
 cd package2
-conan create . --test-folder testpackage
+conan create . --test-folder testpackage --update
 cd -
